ef19d289e4119fcc751d7a6f03c0dd6b2fc8eea6
Jayson Nabor <sonabstudios@gmail.com>
2018-04-28 07:47:47 +0800

    updated phone layout

--- src/components/Profile/phone.vue
+++ src/components/Profile/phone.vue
@@ -1,17 +1,61 @@
 <template>
 <div>
-  <v-subheader class="ma-0 a-0">
-    <v-icon small class="mr-1">phone</v-icon>
-    Phone Numbers
-    <v-spacer></v-spacer>
-    <v-btn icon flat class="pa-0 ma-0" @click="dialog = !dialog">
-      <v-icon v-if="phoneUpdate.mobile === '' && phoneUpdate.business === '' && phoneUpdate.home === ''"
-        color="grey lighten-2">
-        add
-      </v-icon>
-      <v-icon v-else color="grey lighten-2">edit</v-icon>
-    </v-btn>
-  </v-subheader>
+  <v-list-tile>
+    <v-list-tile-content>
+      <v-list-tile-sub-title class="ma-0 pa-0">
+        <v-icon small class="mr-1">phone</v-icon>
+        Phone Numbers
+      </v-list-tile-sub-title>
+    </v-list-tile-content>
+    <v-list-tile-action>
+      <v-btn icon flat class="pa-0 ma-0" @click="dialog = !dialog">
+        <v-icon v-if="fullNameUp !== '  '" color="grey lighten-2">edit</v-icon>
+        <v-icon v-else color="tertiary">add</v-icon>
+      </v-btn>
+    </v-list-tile-action>
+  </v-list-tile>
+  <div class="ma-0 pt-0 pb-2 pl-3">
+    <v-chip>
+      <v-avatar>
+        <v-icon color="grey">mdi-cellphone</v-icon>
+      </v-avatar>
+      {{ phoneUpdate.mobile === ''? '...' :  phoneUpdate.mobile }}
+    </v-chip>
+    <v-chip>
+      <v-avatar>
+        <v-icon color="grey">mdi-deskphone</v-icon>
+      </v-avatar>
+      {{ phoneUpdate.business === ''? '...' :  phoneUpdate.business }}
+    </v-chip>
+    <v-chip>
+      <v-avatar>
+        <v-icon color="grey">mdi-home-variant</v-icon>
+      </v-avatar>
+      {{ phoneUpdate.home === ''? '...' :  phoneUpdate.home }}
+    </v-chip>
+  </div>
+  <div v-if="showEditView" class="pt-2 pl-2 pr-2 pb-2 indigo lighten-5">
+    <v-card-text class="indigo lighten-5">
+      <v-text-field
+        v-model="phoneUpdate.mobile"
+        label="Mobile Number">
+      </v-text-field>
+      <v-text-field
+        v-model="phoneUpdate.business"
+        label="Business Number">
+      </v-text-field>
+      <v-text-field
+        v-model="phoneUpdate.home"
+        label="Home Number">
+      </v-text-field>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn class="ml-4" small :disabled="!enableSave" @click="cancelEdit()">CANCEL</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn small  @click="closeEdit()" >CLOSE</v-btn>
+      <v-btn class="mr-4" small :disabled="!enableSave" @click="updateAttribute()" color="success">SAVE</v-btn>
+    </v-card-actions>
+  </div>
 
   <!--
   <v-list-tile>
